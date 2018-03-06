8c0586bf62574dc5c1b60a5125a963969e332ab7
thanhnt <tatthanh.dev@gmail.com>
2018-03-06 10:02:32 +0700

    fixbug import jquery ui

--- resources/views/layouts/admin/adminlte/scripts.blade.php
+++ resources/views/layouts/admin/adminlte/scripts.blade.php
@@ -1,5 +1,5 @@
 <script src="{!! \URLHelper::asset('libs/plugins/jQuery/jquery-3.2.1.min.js', 'admin') !!}"></script>
-<script src="{!! \URLHelper::asset('libs/plugins/jQueryUI/jQuery-ui.min.js', 'admin') !!}"></script>
+<script src="{!! \URLHelper::asset('libs/plugins/jQueryUI/jquery-ui.min.js', 'admin') !!}"></script>
 
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>$.widget.bridge('uibutton', $.ui.button);</script>
